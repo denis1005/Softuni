bea1db27c627f8e69f574baddd32d1e9bd987f69
denis1005 <denis_1005@abv.bg>
2019-08-13 11:00:34 +0300

    Update

---
+++ C# OOP/Unit Testing/01. Database_Skeleton/Service/Models/Devices/PC.cs
@@ -0,0 +1,24 @@
+﻿using Service.Models.Contracts;
+using Service.Models.Parts;
+using System;
+
+namespace Service.Models.Devices
+{
+    public class PC : Device
+    {
+        public PC(string make)
+            : base(make)
+        {
+        }
+
+        public override void AddPart(IPart part)
+        {
+            if (part.GetType() != typeof(PCPart))
+            {
+                throw new InvalidOperationException($"You cannot add {part.GetType().Name} to {this.GetType().Name}!");
+            }
+
+            base.AddPart(part);
+        }
+    }
+}
---
+++ C# OOP/Unit Testing/01. Database_Skeleton/Service/Models/Devices/Phone.cs
@@ -0,0 +1,23 @@
+﻿using Service.Models.Contracts;
+using Service.Models.Parts;
+using System;
+
+namespace Service.Models.Devices
+{
+    public class Phone : Device
+    {
+        public Phone(string make) : base(make)
+        {
+        }
+
+        public override void AddPart(IPart part)
+        {
+            if (part.GetType() != typeof(PhonePart))
+            {
+                throw new InvalidOperationException($"You cannot add {part.GetType().Name} to {this.GetType().Name}!");
+            }
+
+            base.AddPart(part);
+        }
+    }
+}
